f10bd6d0e1e13ef2b25507d330fc97ee1174255a
a923181 <a923181@02LAB108PC03.rn.senac.br>
2016-07-14 21:10:06 -0300

    4th

--- src/view/ClienteGui.java
+++ src/view/ClienteGui.java
@@ -11,40 +11,20 @@ public class ClienteGui {
     
     public ClienteGui(){}
     
-//    public void cadastrar(){
-//        System.out.print("Nome: ");
-//        String nome = input.next();
-//        System.out.print("CPF: ");
-//        String cpf = input.next();
-//        System.out.print("Sexo: ");
-//        String sexo = input.next();
-//        
-//        ClienteController cc = new ClienteController();
-//        if (cc.cadastrar(nome, cpf, sexo)){
-//            System.out.println("SUCESSO");
-//        }
-//        else {
-//            System.out.println("Falha!");
-//        }
-//    }
-    
-    public static void main(String[] args) {
-       ClienteGui gui = new ClienteGui();
-       gui.buscar();
-//       gui.cadastrar();
-    }
-    
-    public void buscar(){
+    public void cadastrar(){
+        System.out.print("Nome: ");
+        String nome = input.next();
         System.out.print("CPF: ");
         String cpf = input.next();
+        System.out.print("Sexo: ");
+        String sexo = input.next();
         
         ClienteController cc = new ClienteController();
-        
-        if(cc.buscarPorCpf(cpf)){
-            System.out.println("Sucesso");
+        if (cc.cadastrar(nome, cpf, sexo)){
+            System.out.println("SUCESSO");
         }
-        else{
-            System.out.println("Falha");
+        else {
+            System.out.println("Falha!");
         }
     }
 }
